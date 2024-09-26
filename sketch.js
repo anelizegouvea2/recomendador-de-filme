6bfbdafcd78ceb6a04cb5ea4f1eb93c0a53a2811
anelizegouvea2 <161738039+anelizegouvea2@users.noreply.github.com>
2024-09-26 09:24:11 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+// Percy Jackson e o Ladrão de Raios, 12, aventura, fantasia
+// FormiguinhaZ, LIVRE, animação, aventura, familia, comédia
+// Um Tira da Pesada 4, 14, ação e comédia
+// Gente Grande, 12,comédia
+// Os Novatos, 16, comédia
+// Como Eu Era Antes De Você,12, drama, romance
+// O Mal que nos Habita, 18, terror
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
