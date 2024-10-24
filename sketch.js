e443adbfe70a3dc6acaea7a1f7a7b5d00207c7d7
anelizegouvea2 <161738039+anelizegouvea2@users.noreply.github.com>
2024-10-24 10:54:00 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,66 @@
+//Divertida Mente, aventura, familia, L
+//A Caminho da Lua, fantasia, aventura, 10
+//Percy Jackson e o Ladrão de Raios, fantasia, 12
+//Ladrões de Bicicleta, 12
+//O menino que descobriu o vento, 14
+//O Estranho Mundo de Jack, fantasia, L
+//Homem-Aranha: Através do Aranhaverso, aventura, 10
 
-// Percy Jackson e o Ladrão de Raios, 12, aventura, fantasia
-// FormiguinhaZ, LIVRE, animação, aventura, familia, comédia
-// Um Tira da Pesada 4, 14, ação e comédia
-// Gente Grande, 12,comédia
-// Os Novatos, 16, comédia
-// Como Eu Era Antes De Você,12, drama, romance
-// O Mal que nos Habita, 18, terror
+ let campoIdade;
+ let campoFantasia;
+ let campoAventura;
 
 function setup() {
-  createCanvas(400, 400);
-}
+  createCanvas(850,400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("gostaDeFantasia");
+  campoAventura = createCheckbox("Gosta de aventura?");
+  }
 
 function draw() {
-  background(220);
+  background("#169646");
+  let idade= campoIdade. value ();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao=
+geraRecomendacao(idade,gostaDeFantasia, gostaDeAventura);
+  
+  fill(color("#FFFFFF"));
+  textAlign(CENTER,CENTER);
+  textSize(30);
+  text(recomendacao, width / 3, height / 2);
+}
+
+ function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "A Caminho da Lua"; 
+        } else{
+         return "Ladrões de bicicleta";
+        }
+      } else {
+        if (idade >=10)
+        if (gostaDeFantasia) {
+          return "Percy Jackson e o Ladrão de Raios";
+        } else {
+          if (gostaDeAventura)
+          return "Homem-Aranha: Através do Aranhaverso";
+        }
+      }
+    }
+  } else {
+       if (idade <=10)
+    if (gostaDeFantasia) {
+      return "O Estranho Mundo de Jack"; //L
+    } else {
+          if (gostaDeAventura)
+      return "Divertida Mente" //L
+    }
+  }
 }
